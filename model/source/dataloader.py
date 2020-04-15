47ab331eec38eeb90e86eb65138ab11cf2d6744d
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-04-15 17:36:12 +0300

    Create data loaders for X and Y domains

---
+++ model/source/dataloader.py
@@ -0,0 +1,36 @@
+import os
+import torch
+from torch.utils.data import DataLoader
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+
+
+def get_data_loader(image_type, image_dir, shuffle=True,
+                    image_size=128, batch_size=16, num_workers=0):
+    """Returns training and test data loaders for a given image type, either 'summer' or 'winter'. 
+       These images will be resized to 128x128x3, by default, converted into Tensors, and normalized.
+    """
+    
+    # resize and normalize the images
+    # ? Crop to make images square ?
+    transform = transforms.Compose([transforms.Resize(image_size), # resize to 128x128
+                                    transforms.ToTensor()])
+
+    # get training and test directories
+    image_path = os.path.join(image_dir, image_type)
+    #image_path = './' + image_dir
+    #train_path = os.path.join(image_path, image_type)
+    #test_path = os.path.join(image_path, 'test_{}'.format(image_type))
+
+    # define datasets using ImageFolder
+    train_dataset = datasets.ImageFolder(image_path, transform)
+
+    # create and return DataLoaders
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+
+    return train_loader
+
+
+
--- model/source/train.py
+++ model/source/train.py
@@ -1,11 +1,14 @@
-import os
-import torch
-from torch.utils.data import DataLoader
-import torchvision
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
+from dataloader import get_data_loader
 
 
-parser = argparse.ArgumentParser()
-args = parser.parse_args()
-print(args)
+
+# Create train and test dataloaders for images from the two domains X and Y
+# image_type = directory names for our data
+trainloader_X = get_data_loader(image_type='summer', image_dir='../data/train', shuffle=True)
+trainloader_Y = get_data_loader(image_type='winter', image_dir='../data/train', shuffle=True)
+
+batch = next(iter(trainloader_X))
+print(batch)
+#parser = argparse.ArgumentParser()
+#args = parser.parse_args()
+#print(args)
