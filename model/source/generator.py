d99b1f2172ed67c7c7758cd618283c5358f6f128
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-04-28 08:39:52 +0300

    Define the generator

--- model/source/generator.py
+++ model/source/generator.py
@@ -22,3 +22,41 @@ class ResidualBlock(nn.Module):
         out1 = F.relu(self.conv1(x))
         # return a summed output, x + resnet_block(x)  
         return x + self.conv2(out1)
+
+
+
+class CycleGenerator(nn.Module):
+    
+    def __init__(self, conv_dim=64, n_res_blocks=6):
+        super(CycleGenerator, self).__init__()
+
+        # 1. Define the encoder part of the generator
+        self.conv1 = conv(in_channels=3, out_channels=conv_dim, kernel_size=4, stride=2, padding=1, batch_norm=True)
+        self.conv2 = conv(conv_dim, conv_dim*2, 4, 2, 1)
+        self.conv3 = conv(conv_dim*2, conv_dim*4, 4, 2, 1)
+
+        # 2. Define the resnet part of the generator
+        resnet_layers = []
+        for i in range(n_res_blocks):
+            resnet_layers.append(ResidualBlock(conv_dim*4))
+            
+        self.resnet = nn.Sequential(*resnet_layers)
+
+        # 3. Define the decoder part of the generator
+        self.deconv1 = deconv(in_channels=conv_dim*4, out_channels=conv_dim*2, 
+                              kernel_size=4, stride=2, padding=1, batch_norm=True)
+        self.deconv2 = deconv(in_channels=conv_dim*2, out_channels=conv_dim, 
+                              kernel_size=4, stride=2, padding=1, batch_norm=True)
+        self.deconv3 = deconv(conv_dim, 3, 4, 2, 1, batch_norm=False)
+
+    def forward(self, x):
+        """Given an image x, returns a transformed image."""
+        # define feedforward behavior, applying activations as necessary
+        x = F.relu(self.conv1(x)) # (3,128,128) -> (64, 64, 64)
+        x = F.relu(self.conv2(x)) # (64, 64, 64) -> (128, 32, 32)
+        x = F.relu(self.conv3(x)) # (256, 16, 16)
+        x = self.resnet(x) 
+        x = F.relu(self.deconv1(x)) # (256, 16, 16) -> (128, 32, 32)
+        x = F.relu(self.deconv2(x)) # (128, 32, 32) -> (64, 64, 64)
+        x = F.tanh(self.deconv3(x)) # (64, 64, 64) -> (3, 128, 128)
+        return x
