1bc4c50846e31eeb25d2ec075424382a356a5d1b
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-05-19 07:51:13 +0300

    Implement checkpoint saving

--- model/source/helpers.py
+++ model/source/helpers.py
@@ -1,6 +1,7 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import os
 
 def scale(x, feature_range=(-1, 1)):
     ''' Scale takes in an image x and returns that image, scaled
@@ -67,3 +68,19 @@ def print_models(G_XtoY, G_YtoX, D_X, D_Y):
     print("-----------------------------------------------")
     print(D_Y)
     print()
+
+
+
+def checkpoint(iteration, G_XtoY, G_YtoX, D_X, D_Y, checkpoint_dir='checkpoints_cyclegan'):
+    """
+    Saves the parameters of both generators G_YtoX, G_XtoY and discriminators D_X, D_Y.
+    """
+    G_XtoY_path = os.path.join(checkpoint_dir, 'G_XtoY.pkl')
+    G_YtoX_path = os.path.join(checkpoint_dir, 'G_YtoX.pkl')
+    D_X_path = os.path.join(checkpoint_dir, 'D_X.pkl')
+    D_Y_path = os.path.join(checkpoint_dir, 'D_Y.pkl')
+    torch.save(G_XtoY.state_dict(), G_XtoY_path)
+    torch.save(G_YtoX.state_dict(), G_YtoX_path)
+    torch.save(D_X.state_dict(), D_X_path)
+    torch.save(D_Y.state_dict(), D_Y_path)
+
