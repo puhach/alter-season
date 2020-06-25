a18544c57a0d176dd30319518aee3c013cb72ce6
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-06-25 17:55:03 +0300

    Change one epoch to be one pass over the entire dataset

--- model/source/train.py
+++ model/source/train.py
@@ -43,148 +43,163 @@ def train(train_dataloader_X, train_dataloader_Y,
 
     for epoch in range(1, n_epochs+1):
 
-        # Reset iterators for each epoch
-        if epoch % batches_per_epoch == 0:
-            iter_X = iter(train_dataloader_X)
-            iter_Y = iter(train_dataloader_Y)
+        epoch_loss_d_x = 0
+        epoch_loss_d_y = 0
+        epoch_loss_g = 0
 
-        # move images to GPU or CPU depending on what is passed in the device parameter
-        # make sure to scale to a range -1 to 1
-        images_X, _ = next(iter_X)
-        images_X = scale(images_X.to(device))
-        #images_X, _ = iter_X.next()
-        #images_X = scale(images_X) 
+        for _ in range(batches_per_epoch):
 
-        #images_Y, _ = iter_Y.next()
-        images_Y, _ = next(iter_Y)
-        #images_Y = scale(images_Y)        
-        images_Y = scale(images_Y.to(device))
+            ## Reset iterators for each epoch
+            #if epoch % batches_per_epoch == 0:
+            #    iter_X = iter(train_dataloader_X)
+            #    iter_Y = iter(train_dataloader_Y)
 
-        # move images to GPU or CPU depending on what is passed in the device parameter
-        #images_X = images_X.to(device)
-        #images_Y = images_Y.to(device)
+            # move images to GPU or CPU depending on what is passed in the device parameter
+            # make sure to scale to a range -1 to 1
+            images_X, _ = next(iter_X)
+            images_X = scale(images_X.to(device))
+            #images_X, _ = iter_X.next()
+            #images_X = scale(images_X) 
+
+            #images_Y, _ = iter_Y.next()
+            images_Y, _ = next(iter_Y)
+            #images_Y = scale(images_Y)        
+            images_Y = scale(images_Y.to(device))
+
+            # move images to GPU or CPU depending on what is passed in the device parameter
+            #images_X = images_X.to(device)
+            #images_Y = images_Y.to(device)
 
 
-        # ============================================
-        #            TRAIN THE DISCRIMINATORS
-        # ============================================
+            # ============================================
+            #            TRAIN THE DISCRIMINATORS
+            # ============================================
+            
+            ##   First: D_X, real and fake loss components   ##
+
+            # Compute the discriminator losses on real images
+            d_x_out = D_X(images_X)
+            d_x_loss_real = real_mse_loss(d_x_out)
+            #d_x_loss_real = bce_loss_crit(d_x_out, torch.ones_like(d_x_out))
+            #d_x_loss_real = torch.nn.functional.binary_cross_entropy_with_logits(d_x_out, torch.ones_like(d_x_out))
+            
+            # Generate fake images that look like domain X based on real images in domain Y
+            fake_x = G_YtoX(images_Y)
+
+            # Compute the fake loss for D_X
+            d_x_out = D_X(fake_x)
+            d_x_loss_fake = fake_mse_loss(d_x_out)
+            #d_x_loss_fake = bce_loss_crit(d_x_out, torch.zeros_like(d_x_out))
+            #d_x_loss_fake = torch.nn.functional.binary_cross_entropy_with_logits(d_x_out, torch.zeros_like(d_x_out))
+            
+            # Compute the total loss
+            d_x_loss = d_x_loss_real + d_x_loss_fake
+            
+
+            
+            ##   Second: D_Y, real and fake loss components   ##
+            
+            d_y_out = D_Y(images_Y) 
+            d_y_real_loss = real_mse_loss(d_y_out)  # D_y disciminator loss on a real Y image
+            #d_y_real_loss = bce_loss_crit(d_y_out, torch.ones_like(d_y_out))
+            #d_y_real_loss = torch.nn.functional.binary_cross_entropy_with_logits(d_y_out, torch.ones_like(d_y_out))
+            
+            fake_y = G_XtoY(images_X) # generate fake Y image from the real X image
+            d_y_out = D_Y(fake_y)
+            d_y_fake_loss = fake_mse_loss(d_y_out) # compute D_y loss on a fake Y image
+            #d_y_fake_loss = bce_loss_crit(d_y_out, torch.zeros_like(d_y_out))
+            #d_y_fake_loss = torch.nn.functional.binary_cross_entropy_with_logits(d_y_out, torch.zeros_like(d_y_out))
+            
+            d_y_loss = d_y_real_loss + d_y_fake_loss
+            
+
+            d_total_loss = d_x_loss + d_y_loss
+
+
+            # =========================================
+            #            TRAIN THE GENERATORS
+            # =========================================
+
+            ##    First: generate fake X images and reconstructed Y images    ##
+
+            # Generate fake images that look like domain X based on real images in domain Y
+            fake_x = G_YtoX(images_Y)
+
+            # Compute the generator loss based on domain X
+            d_out = D_X(fake_x)
+            g_x_loss = real_mse_loss(d_out) # fake X should trick the D_x
+            # TODO: consider using MSELoss or SmoothL1Loss (Huber loss)
+
+            # Create a reconstructed y
+            y_hat = G_XtoY(fake_x)
+                    
+            # Compute the cycle consistency loss (the reconstruction loss)
+            rec_y_loss = cycle_consistency_loss(images_Y, y_hat, lambda_weight=reconstruction_weight)
+
+            # Conversion from X to X should be an identity mapping
+            it_x = G_YtoX(images_X)
+
+            # Compute the identity mapping loss
+            it_x_loss = identity_mapping_loss(images_X, it_x, weight=identity_weight)
+
+
+            ##    Second: generate fake Y images and reconstructed X images    ##
+            fake_y = G_XtoY(images_X)
+            
+            d_out = D_Y(fake_y)
+            g_y_loss = real_mse_loss(d_out)  # fake Y should trick the D_y
+            
+            x_hat = G_YtoX(fake_y)
+            
+            rec_x_loss = cycle_consistency_loss(images_X, x_hat, lambda_weight=reconstruction_weight)
+
+            it_y = G_XtoY(images_Y)
+
+            it_y_loss = identity_mapping_loss(images_Y, it_y, weight=identity_weight)
+
+
+            # Add up all generator and reconstructed losses 
+            g_total_loss = g_x_loss + g_y_loss + rec_x_loss + rec_y_loss + it_x_loss + it_y_loss
+            
+
+            # Perform backprop
+            
+            if d_total_loss >= 0.01*g_total_loss:
+                d_x_optimizer.zero_grad()
+                d_x_loss.backward()
+                d_x_optimizer.step()
+
+                d_y_optimizer.zero_grad()
+                d_y_loss.backward()
+                d_y_optimizer.step()
+            
+            if g_total_loss >= 0.01*d_total_loss:
+                g_optimizer.zero_grad()
+                g_total_loss.backward()
+                g_optimizer.step()
+
+            # Gather statistics
+            epoch_loss_d_x += d_x_loss.item()
+            epoch_loss_d_y += d_y_loss.item()
+            epoch_loss_g += g_total_loss.item()
         
-        ##   First: D_X, real and fake loss components   ##
+        # Reset the iterators when epoch ends
+        iter_X = iter(train_dataloader_X)
+        iter_Y = iter(train_dataloader_Y)
 
-        # 1. Compute the discriminator losses on real images
-        d_x_out = D_X(images_X)
-        d_x_loss_real = real_mse_loss(d_x_out)
-        #d_x_loss_real = bce_loss_crit(d_x_out, torch.ones_like(d_x_out))
-        #d_x_loss_real = torch.nn.functional.binary_cross_entropy_with_logits(d_x_out, torch.ones_like(d_x_out))
-        
-        # 2. Generate fake images that look like domain X based on real images in domain Y
-        fake_x = G_YtoX(images_Y)
-
-        # 3. Compute the fake loss for D_X
-        d_x_out = D_X(fake_x)
-        d_x_loss_fake = fake_mse_loss(d_x_out)
-        #d_x_loss_fake = bce_loss_crit(d_x_out, torch.zeros_like(d_x_out))
-        #d_x_loss_fake = torch.nn.functional.binary_cross_entropy_with_logits(d_x_out, torch.zeros_like(d_x_out))
-        
-        # 4. Compute the total loss
-        d_x_loss = d_x_loss_real + d_x_loss_fake
-        
-
-        
-        ##   Second: D_Y, real and fake loss components   ##
-        
-        d_y_out = D_Y(images_Y) 
-        d_y_real_loss = real_mse_loss(d_y_out)  # D_y disciminator loss on a real Y image
-        #d_y_real_loss = bce_loss_crit(d_y_out, torch.ones_like(d_y_out))
-        #d_y_real_loss = torch.nn.functional.binary_cross_entropy_with_logits(d_y_out, torch.ones_like(d_y_out))
-        
-        fake_y = G_XtoY(images_X) # generate fake Y image from the real X image
-        d_y_out = D_Y(fake_y)
-        d_y_fake_loss = fake_mse_loss(d_y_out) # compute D_y loss on a fake Y image
-        #d_y_fake_loss = bce_loss_crit(d_y_out, torch.zeros_like(d_y_out))
-        #d_y_fake_loss = torch.nn.functional.binary_cross_entropy_with_logits(d_y_out, torch.zeros_like(d_y_out))
-        
-        d_y_loss = d_y_real_loss + d_y_fake_loss
-        
-
-        d_total_loss = d_x_loss + d_y_loss
-
-
-        # =========================================
-        #            TRAIN THE GENERATORS
-        # =========================================
-
-        ##    First: generate fake X images and reconstructed Y images    ##
-
-        # Generate fake images that look like domain X based on real images in domain Y
-        fake_x = G_YtoX(images_Y)
-
-        # Compute the generator loss based on domain X
-        d_out = D_X(fake_x)
-        g_x_loss = real_mse_loss(d_out) # fake X should trick the D_x
-        # TODO: consider using MSELoss or SmoothL1Loss (Huber loss)
-
-        # Create a reconstructed y
-        y_hat = G_XtoY(fake_x)
-                
-        # Compute the cycle consistency loss (the reconstruction loss)
-        rec_y_loss = cycle_consistency_loss(images_Y, y_hat, lambda_weight=reconstruction_weight)
-
-        # Conversion from X to X should be an identity mapping
-        it_x = G_YtoX(images_X)
-
-        # Compute the identity mapping loss
-        it_x_loss = identity_mapping_loss(images_X, it_x, weight=identity_weight)
-
-
-        ##    Second: generate fake Y images and reconstructed X images    ##
-        fake_y = G_XtoY(images_X)
-        
-        d_out = D_Y(fake_y)
-        g_y_loss = real_mse_loss(d_out)  # fake Y should trick the D_y
-        
-        x_hat = G_YtoX(fake_y)
-        
-        rec_x_loss = cycle_consistency_loss(images_X, x_hat, lambda_weight=reconstruction_weight)
-
-        it_y = G_XtoY(images_Y)
-
-        it_y_loss = identity_mapping_loss(images_Y, it_y, weight=identity_weight)
-
-
-        # Add up all generator and reconstructed losses 
-        g_total_loss = g_x_loss + g_y_loss + rec_x_loss + rec_y_loss + it_x_loss + it_y_loss
-        
-
-
-        # Perform backprop
-
-        
-        if d_total_loss >= 0.01*g_total_loss:
-            d_x_optimizer.zero_grad()
-            d_x_loss.backward()
-            d_x_optimizer.step()
-
-            d_y_optimizer.zero_grad()
-            d_y_loss.backward()
-            d_y_optimizer.step()
-        
-        if g_total_loss >= 0.01*d_total_loss:
-            g_optimizer.zero_grad()
-            g_total_loss.backward()
-            g_optimizer.step()
-
-        
         # Print the log info
-        if epoch % print_every == 0:
+        if epoch % print_every == 0 or epoch == n_epochs:
             # append real and fake discriminator losses and the generator loss
-            losses.append((d_x_loss.item(), d_y_loss.item(), g_total_loss.item()))
+            losses.append((epoch_loss_d_x, epoch_loss_d_y, epoch_loss_g))
             print('Epoch [{:5d}/{:5d}] | d_X_loss: {:6.4f} | d_Y_loss: {:6.4f} | g_total_loss: {:6.4f}'.format(
-                    epoch, n_epochs, d_x_loss.item(), d_y_loss.item(), g_total_loss.item()))
+                    epoch, n_epochs, epoch_loss_d_x, epoch_loss_d_y, epoch_loss_g))
+            #losses.append((d_x_loss.item(), d_y_loss.item(), g_total_loss.item()))
+            #print('Epoch [{:5d}/{:5d}] | d_X_loss: {:6.4f} | d_Y_loss: {:6.4f} | g_total_loss: {:6.4f}'.format(
+            #        epoch, n_epochs, d_x_loss.item(), d_y_loss.item(), g_total_loss.item()))
 
             
         # Save the generated samples
-        if epoch % sample_every == 0:
+        if epoch % sample_every == 0 or epoch == n_epochs:
             G_YtoX.eval() # set generators to eval mode for sample generation
             G_XtoY.eval()
             #save_samples(epoch, fixed_Y, fixed_X, G_YtoX, G_XtoY, device=device, batch_size=16, sample_dir='../samples')
