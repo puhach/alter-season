81490613eeeb961e4e4e237c405a778a1ad2c6a5
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-07-25 15:54:29 +0300

    Set the user-visible name of the application

--- app/alter-season-qt-widgets/main.cpp
+++ app/alter-season-qt-widgets/main.cpp
@@ -5,7 +5,10 @@
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
+	app.setApplicationDisplayName(app.tr("Alter Season"));
+
 	MainWindow mainWindow;
 	mainWindow.show();
+
 	return app.exec();
 }
