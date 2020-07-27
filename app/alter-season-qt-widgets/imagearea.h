e219e2048897e5f4327473fa009b2f1d093aebc4
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-07-27 10:47:50 +0300

    Add a function to retrieve the inscription text from the image area

--- app/alter-season-qt-widgets/imagearea.h
+++ app/alter-season-qt-widgets/imagearea.h
@@ -11,10 +11,14 @@ class ImageArea : public QLabel
 public:
 	ImageArea(const QString &inscription, QWidget* parent = nullptr);
 
+	QString getInscription() const { return inscription; }
+
 	void showMessage(const QString& message, int duration);
 	void showImage(const QImage &image);
 
 	//virtual QSize sizeHint() const;
+
+
 protected:
 	//virtual void dragEnterEvent(QDragEnterEvent* event) override;
 	//virtual void dropEvent(QDropEvent* event) override;
