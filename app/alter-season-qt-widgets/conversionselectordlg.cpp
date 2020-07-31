4081ef35885216ad12574188a023ec60c3ce352d
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-07-31 10:32:48 +0300

    Add the conversion selector

---
+++ app/alter-season-qt-widgets/conversionselectordlg.cpp
@@ -0,0 +1,5 @@
+#include "conversionselectordlg.h"
+
+ConversionSelectorDlg::ConversionSelectorDlg(QWidget* parent): QDialog(parent)
+{
+}
---
+++ app/alter-season-qt-widgets/conversionselectordlg.h
@@ -0,0 +1,12 @@
+#ifndef CONVERSIONSELECTORDLG_H
+#define CONVERSIONSELECTORDLG_H
+
+#include <QDialog>
+
+class ConversionSelectorDlg : public QDialog
+{
+public:
+	ConversionSelectorDlg(QWidget* parent = nullptr);
+};
+
+#endif // CONVERSIONSELECTORDLG_H
