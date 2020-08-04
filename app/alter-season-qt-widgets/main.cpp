cd742e7da7d1e01f284f6df1bd8b732c17f3a409
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-08-04 08:03:12 +0300

    Handle exceptions from the main window constructor

--- app/alter-season-qt-widgets/main.cpp
+++ app/alter-season-qt-widgets/main.cpp
@@ -1,14 +1,36 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QMessageBox>
+
+//#include <iostream>
+//
+//#include <QDir>	// TEST!
+//#include <QDebug>
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 	app.setApplicationDisplayName(app.tr("Alter Season"));
 
-	MainWindow mainWindow;
-	mainWindow.show();
+	// Throwing exceptions from an event handler is not supported in Qt. Reimplement QApplication::notify() 
+	// and catch all exceptions there.
+	// https://stackoverflow.com/questions/10075792/how-to-catch-exceptions-in-qt
+	// https://forum.qt.io/topic/17263/solved-problems-with-reimplementing-qapplication-notify-for-exception-handling/2
 
-	return app.exec();
+	try
+	{		
+		MainWindow mainWindow;
+		mainWindow.show();
+
+		return app.exec();
+	}
+	catch (const std::exception& e)
+	{
+		//qDebug() << QDir::currentPath();
+		//qDebug() << e.what();
+		QMessageBox::critical(nullptr, QObject::tr("Error"), QString::fromStdString(e.what()));
+		//std::cerr << e.what() << std::endl;
+		return -1;
+	}
 }
